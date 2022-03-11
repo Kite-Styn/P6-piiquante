06d779c50ca40b6dfa8553f75d025b0d6310e6e8
Kite-Styn <tonyci0402@gmail.com>
2022-03-11 18:05:00 +0100

    routes post and get with auth

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
 require ("dotenv").config();
+const helmet = require("helmet");
 
 const userRoutes = require("./routes/user");
+const sauceRoutes = require("./routes/sauce");
 
 const app = express();
+
+app.use(helmet());
 
 mongoose.connect(`mongodb+srv://${process.env.DEV_USER}:${process.env.DEV_PASS}@cluster0.jx7ax.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,{ useNewUrlParser: true,useUnifiedTopology: true })
 .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -18,11 +22,15 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
     next();
 });
 
 app.use(bodyParser.json());
 
+app.use("/images", express.static(path.join(__dirname, "images")));
+
 app.use("/api/auth", userRoutes);
+app.use("/api/sauces", sauceRoutes);
 
 module.exports = app;
--- controllers/user.js
+++ controllers/user.js
@@ -11,7 +11,7 @@ exports.signup = (req, res, next) => {
             password: hash
         });
         user.save()
-        .then(() => res.status(201).json({message: "Utilisateur créé!"}))
+        .then(() => res.status(201).json({message: "Utilisateur créé"}))
         .catch(error => res.status(400).json({error}));
     })
     .catch(error => res.status(500).json({error}));
@@ -21,7 +21,7 @@ exports.login = (req, res, next) => {
     User.findOne({email: req.body.email})
     .then(user => {
         if (!user) {
-            return res.status(401).json({message: "Utilisateur non trouvé!"})
+            return res.status(401).json({message: "Utilisateur non trouvé"})
         }
         bcrypt.compare(req.body.password, user.password)
         .then(valid => {
